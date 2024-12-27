36a8cf6a1f611bf2e9f23943b4503ca1ed3bb2ed
Werner Dietl <wdietl@gmail.com>
2024-12-26 19:23:06 -0500

    Suppress a warning

--- src/main/java/org/plumelib/reflection/Signatures.java
+++ src/main/java/org/plumelib/reflection/Signatures.java
@@ -645,6 +645,7 @@ public final class Signatures {
    * @param internalForm a class name in internal form
    * @return the class name as dot-separated identifiers
    */
+  @SuppressWarnings("signature:return.type.incompatible") // TODO with checker update
   public static @DotSeparatedIdentifiers String internalFormToDotSeparatedIdentifiers(
       @InternalForm String internalForm) {
     return internalForm.replace('/', '.');
